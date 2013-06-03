6c4f8c63c88faa83e64f756849ade9ad038b2b46
John Else <john.else@citrix.com>
2013-06-03 01:51:18 +0100

    Add some helper functions to deal with floats
    Signed-off-by: John Else <john.else@citrix.com>

--- lib/osc_message.ml
+++ lib/osc_message.ml
@@ -3,3 +3,14 @@ type argument =
   | Float32 of float
   | Str of string
   | Blob of string
+
+let bits_of_float f =
+  BITSTRING {
+    Int32.bits_of_float f : 32 : bigendian
+  }
+
+let float_of_bits b =
+  bitmatch b with {
+    i : 4*8 : bigendian
+  } ->
+    Int32.float_of_bits i
