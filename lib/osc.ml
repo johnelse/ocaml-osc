1ad4bbfa7761619741c1d0fb68d59c02c613faa0
John Else <john.else@citrix.com>
2013-06-15 10:53:44 +0100

    Add functions for reading and encoding blobs
    Signed-off-by: John Else <john.else@citrix.com>

--- lib/osc.ml
+++ lib/osc.ml
@@ -13,6 +13,11 @@ type packet =
   | Message of message
   | Bundle of packet list
 
+let blob_padding_of_length length =
+  match length mod 4 with
+  | 0 -> 0
+  | x -> 4 - x
+
 let encode_float f =
   BITSTRING {
     Int32.bits_of_float f : 32 : bigendian
@@ -30,6 +35,22 @@ let encode_string s =
   let padding = 4 - (length mod 4) in
   let suffix = String.make padding '\000' in
   Bitstring.bitstring_of_string (s ^ suffix)
+
+let encode_blob b =
+  let length = String.length b in
+  let padding = blob_padding_of_length length in
+  if padding = 0 then
+    BITSTRING {
+      (Int32.of_int length) : 32 : bigendian;
+      b : length * 8 : string
+    }
+  else
+    let suffix = String.make padding '\000' in
+    BITSTRING {
+      (Int32.of_int length) : 32 : bigendian;
+      b : length * 8 : string;
+      suffix : padding * 8 : string
+    }
 
 let read_float data =
   bitmatch data with {
@@ -64,3 +85,12 @@ let read_string data =
   in
   read_string' (Buffer.create 16) data
 
+let read_blob data =
+  let size, rest = read_int32 data in
+  let blob_length = Int32.to_int size in
+  let padding_length = blob_padding_of_length blob_length in
+  bitmatch rest with {
+    b : blob_length * 8 : string;
+    _ : padding_length * 8 : string;
+    rest : -1 : bitstring
+  } -> b, rest
