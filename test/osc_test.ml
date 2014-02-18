d3efbd4b75b72c390ba4f5dca0ce8d8fbeafcfe7
John Else <john.else@citrix.com>
2014-02-18 05:26:11 +0000

    Add a localhost UDP test
    Signed-off-by: John Else <john.else@citrix.com>

--- test/osc_test.ml
+++ test/osc_test.ml
@@ -60,10 +60,38 @@ let test_string_codec () =
   let received_message_packet = Osc_string.to_packet data in
   assert_packets_equal test_message_packet received_message_packet
 
+
+let test_unix_udp_send_recv () =
+  let open Osc_unix.Udp in
+  let localhost = Unix.inet_addr_of_string "127.0.0.1" in
+  let port = 4567 in
+  let addr = Unix.ADDR_INET (localhost, port) in
+  let buffer_length = 1024 in
+  bracket
+    (fun () ->
+      let client = Client.create () in
+      let server = Server.create addr buffer_length in
+      client, server)
+    (fun (client, server) ->
+      let server_receive_thread channel =
+        let packet = Server.recv server in
+        Event.sync (Event.send channel packet)
+      in
+      let channel = Event.new_channel () in
+      let (_: Thread.t) = Thread.create server_receive_thread channel in
+      Client.send client addr test_message_packet;
+      let received_message_packet = Event.sync (Event.receive channel) in
+      assert_packets_equal test_message_packet received_message_packet)
+    (fun (client, server) ->
+      Client.destroy client;
+      Server.destroy server)
+    ()
+
 let base_suite =
   "base_suite" >:::
     [
       "test_string_codec" >:: test_string_codec;
+      "test_unix_udp_send_recv" >:: test_unix_udp_send_recv;
     ]
 
 let _ = run_test_tt_main base_suite
