7633d08d83b89e61f8052ac39e0b5a55bc336fad
John Else <john.else@citrix.com>
2014-06-22 18:43:43 +0100

    Reword a doccomment
    Signed-off-by: John Else <john.else@citrix.com>

--- lib/osc_string.mli
+++ lib/osc_string.mli
@@ -1,7 +1,7 @@
 (** Conversion of OSC packets to and from strings. *)
 
 val of_packet : Osc.packet -> string
-(** Convert an OSC packet to a string. *)
+(** Serialise an OSC packet into a string. *)
 
 val to_packet : string -> Osc.packet
 (** Attempt to deserialise a string into an OSC packet. *)
