dcf50a3f0fd77b747455bafa9fe3887826598d63
John Else <john.else@citrix.com>
2014-06-30 10:25:39 +0100

    Revert "Add SuperCollider interop test to travis"
    Travis VMs don't have a new enough supercollider to be able to use
    OSCFunc.
    
    This reverts commit 8148a008bd9a4b6327e47850d3b4f227f5cace9a.

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -15,7 +15,6 @@ esac
 echo "yes" | sudo add-apt-repository ppa:$ppa
 sudo apt-get update -qq
 sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
-sudo apt-get install -qq supercollider-language
 export OPAMYES=1
 export OPAMVERBOSE=1
 echo OCaml version
@@ -34,5 +33,4 @@ fi
 eval `opam config -env`
 make
 make test
-make test-interop-sclang
 make doc
