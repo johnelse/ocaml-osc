15400f94f2e8116bfaa8487fcf790538823a5361
John Else <john.else@citrix.com>
2014-07-08 20:03:38 +0100

    Re-add SuperCollider interop tests to Travis script
    Signed-off-by: John Else <john.else@citrix.com>

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -15,6 +15,7 @@ esac
 echo "yes" | sudo add-apt-repository ppa:$ppa
 sudo apt-get update -qq
 sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
+sudo apt-get install -qq supercollider
 export OPAMYES=1
 export OPAMVERBOSE=1
 echo OCaml version
@@ -33,4 +34,5 @@ fi
 eval `opam config -env`
 make
 make test
+make test-interop-sclang
 make doc
