16731a499c3cd2c7cbc2467fb94753dec90e0e77
John Else <john.else@citrix.com>
2014-11-06 14:00:55 +0000

    Run tests against OPAM 1.2.0 only
    Signed-off-by: John Else <john.else@citrix.com>

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,15 +1,11 @@
 # OPAM packages needed to build tests.
 OPAM_PACKAGES="ocplib-endian ounit"
 
-case "$OCAML_VERSION,$OPAM_VERSION" in
-3.12.1,1.0.0) ppa=avsm/ocaml312+opam10 ;;
-3.12.1,1.1.0) ppa=avsm/ocaml312+opam11 ;;
-4.00.1,1.0.0) ppa=avsm/ocaml40+opam10 ;;
-4.00.1,1.1.0) ppa=avsm/ocaml40+opam11 ;;
-4.01.0,1.0.0) ppa=avsm/ocaml41+opam10 ;;
-4.01.0,1.1.0) ppa=avsm/ocaml41+opam11 ;;
-4.02.0,1.1.0) ppa=avsm/ocaml42+opam11 ;;
-*) echo Unknown $OCAML_VERSION,$OPAM_VERSION; exit 1 ;;
+case "$OCAML_VERSION" in
+4.00.1) ppa=avsm/ocaml40+opam12 ;;
+4.01.0) ppa=avsm/ocaml41+opam11 ;;
+4.02.1) ppa=avsm/ocaml42+opam12 ;;
+*) echo Unknown $OCAML_VERSION; exit 1 ;;
 esac
 
 echo "yes" | sudo add-apt-repository ppa:$ppa
@@ -31,5 +27,5 @@ then
     opam install lwt
 fi
 
-eval `opam config -env`
+eval `opam config env`
 make doc test test-interop-sclang
