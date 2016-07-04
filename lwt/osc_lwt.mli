4d80198546037442c641b550c008e1a2512e46a8
John Else <john.else@citrix.com>
2016-07-04 14:27:26 +0100

    Document the Osc_lwt and Osc_unix libraries
    Signed-off-by: John Else <john.else@citrix.com>

--- lwt/osc_lwt.mli
+++ lwt/osc_lwt.mli
@@ -1,20 +1,30 @@
+(** OSC transport library using Lwt IO. *)
+
+(** OSC communication over UDP. *)
 module Udp : sig
   module Client : sig
     type t
+    (** The type of a UDP Lwt client. *)
 
     val create : unit -> t Lwt.t
+    (** Create a new client. *)
 
     val destroy : t -> unit Lwt.t
+    (** Destroy a client. *)
 
     val send : t -> Lwt_unix.sockaddr -> Osc.Types.packet -> unit Lwt.t
+    (** Send an OSC packet to a specified address. *)
   end
 
   module Server : sig
     type t
+    (** The type of a UDP Lwt server. *)
 
     val create : Lwt_unix.sockaddr -> int -> t Lwt.t
+    (** Create a new server. *)
 
     val destroy : t -> unit Lwt.t
+    (** Destroy a server. *)
 
     val recv :
       t ->
@@ -22,5 +32,6 @@ module Udp : sig
         | `Missing_typetag_string
         | `Unsupported_typetag of char
       ]) Result.result) Lwt.t
+    (** Attempt to receive the next packet. *)
   end
 end
--- unix/osc_unix.mli
+++ unix/osc_unix.mli
@@ -1,20 +1,30 @@
+(** OSC transport library using Unix IO. *)
+
+(** OSC communication over UDP. *)
 module Udp : sig
   module Client : sig
     type t
+    (** The type of a UDP Unix client. *)
 
     val create : unit -> t
+    (** Create a new client. *)
 
     val destroy : t -> unit
+    (** Destroy a client. *)
 
     val send : t -> Unix.sockaddr -> Osc.Types.packet -> unit
+    (** Send an OSC packet to a specified address. *)
   end
 
   module Server : sig
     type t
+    (** The type of a UDP Unix server. *)
 
     val create : Unix.sockaddr -> int -> t
+    (** The type of a UDP Lwt server. *)
 
     val destroy : t -> unit
+    (** Destroy a server. *)
 
     val recv :
       t ->
@@ -22,5 +32,6 @@ module Udp : sig
         | `Missing_typetag_string
         | `Unsupported_typetag of char
       ]) Result.result
+    (** Attempt to receive the next packet. *)
   end
 end
