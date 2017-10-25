b34c9e4e5eff2984a2829453135c2247983897a2
John Else <john.else@gmail.com>
2017-10-25 22:01:27 +0100

    Use xvfb-run for the interop tests, if available
    Newer versions of sclang don't start up properly if no X server is
    available.
    
    Signed-off-by: John Else <john.else@gmail.com>

--- test/test-interop-sclang.sh
+++ test/test-interop-sclang.sh
@@ -7,7 +7,13 @@ SCLANG=`which sclang`
 
 if [ $? -eq 0 ]
 then
-    ./test_interop_sclang.byte $ML_PORT $SCLANG $SC_PORT test/EchoServer.sc
+    XVFB_RUN=`which xvfb-run`
+    if [ $? -eq 0 ]
+    then
+        $XVFB_RUN ./test_interop_sclang.byte $ML_PORT $SCLANG $SC_PORT test/EchoServer.sc
+    else
+        ./test_interop_sclang.byte $ML_PORT $SCLANG $SC_PORT test/EchoServer.sc
+    fi
 else
     echo "couldn't find sclang"
     exit 1
