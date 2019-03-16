d7fffa22a6bd6ce35cad2c8c64466f4b1c48ab4f
John Else <john.else@gmail.com>
2019-03-16 17:47:08 +0000

    Add the SuperCollider interop test to dune
    Signed-off-by: John Else <john.else@gmail.com>

--- test/unix/helpers/test-interop-sclang.sh
+++ test/unix/helpers/test-interop-sclang.sh
@@ -10,9 +10,9 @@ then
     XVFB_RUN=`which xvfb-run`
     if [ $? -eq 0 ]
     then
-        $XVFB_RUN ./test_interop_sclang.exe $ML_PORT $SCLANG $SC_PORT test/EchoServer.sc
+        $XVFB_RUN ./test_interop_sclang.exe $ML_PORT $SCLANG $SC_PORT helpers/EchoServer.sc
     else
-        ./test_interop_sclang.exe $ML_PORT $SCLANG $SC_PORT test/EchoServer.sc
+        ./test_interop_sclang.exe $ML_PORT $SCLANG $SC_PORT helpers/EchoServer.sc
     fi
 else
     echo "couldn't find sclang"
